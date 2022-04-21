e29436774010b8b8c4ef565012cb550ca1b98d0a
user <gol_iol_123>
2022-04-21 18:32:53 +0300

    commit

--- src/main/java/com/stackabuse/postgresql/CustomerApplication.java
+++ src/main/java/com/stackabuse/postgresql/CustomerApplication.java
@@ -11,13 +11,13 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import java.util.HashMap;
+import java.util.Map;
 /**
  *
  * @author Hiram K. <https://github.com/IdelsTak>
  */
 public class CustomerApplication {
-    
     private static final Logger LOGGER = Logger.getLogger(CustomerApplication.class.getName());
     private static final Dao<Customer, Integer> CUSTOMER_DAO = new PostgreSqlDao();
 
@@ -82,124 +82,233 @@ public class CustomerApplication {
     }
     public static void add_trans()
     {
-        String buff = "";
-        Scanner in = new Scanner(System.in);
-        Integer customer_id = 0;
-        String tr_datetime = "";
-        Integer mcc_code = 0;
-        Integer tr_type = 0;
-        Integer amount = 0;
-        Integer term_id = 0;
-        boolean check = false;
-
-        System.out.println("Введите id работника");
-        customer_id = input_int("table_1", "customer_id", "int");
-        System.out.println("Введите дату транзакции");
-        System.out.println("Введите МСС код");
-        mcc_code = input_int("table_1", "mcc_code", "int");
-        System.out.println("Введите тип транзакции");
-        tr_type = input_int("table_1", "tr_type", "int");
-        System.out.println("Введите сумму операции");
-        System.out.println("Введите id терминала");
-        term_id = input_int("table_1", "term_id", "int");
-        // проверить, что такой id есть
-
-        Transaction trans = new Transaction(customer_id, tr_datetime, mcc_code, tr_type, amount, term_id);
-
         // добавить транзакция
     }
-    public static Transaction get_trans(int id)
+    public static Transaction get_trans()
     {
         // получить транзакцию
         Transaction trans = new Transaction(); // get
         return trans;
     }
-    public static void update_trans(int id)
+    public static void update_trans()
     {
         // получить транзакцию
-        String buff = "";
-        Scanner in = new Scanner(System.in);
-        Integer customer_id = 0;
-        String tr_datetime = "";
-        Integer mcc_code = 0;
-        Integer tr_type = 0;
-        Integer amount = 0;
-        Integer term_id = 0;
-        boolean check = false;
 
-        System.out.println("Введите id работника");
-        customer_id = input_int("table_1", "customer_id", "int");
-        System.out.println("Введите дату транзакции");
-        System.out.println("Введите МСС код");
-        mcc_code = input_int("table_1", "mcc_code", "int");
-        System.out.println("Введите тип транзакции");
-        tr_type = input_int("table_1", "tr_type", "int");
-        System.out.println("Введите сумму операции");
-        System.out.println("Введите id терминала");
-        term_id = input_int("table_1", "term_id", "int");
     }
-    public static boolean kill_trans(int id)
+    public static void kill_trans()
     {
-        // удалить транзакцию
-        Integer customer_id = 0;
-        System.out.println("Введите id работника");
-        customer_id = input_int("table_1", "customer_id", "int");
-        // delete
-        return true;
+
+    }
+
+    public static void load_trans()
+    {
+
+    }
+    public static void save_trans()
+    {
+
     }
     /////////
     public static void set_gender()
     {
-        // добавить транзакция
-        Integer customer_id = 0;
-        Integer gender = 0;
-        String buff = "";
-        System.out.println("Введите id работника");
-        customer_id = input_int("table_1", "customer_id", "int");
-        System.out.println("Введите пол работника");
-        Scanner in = new Scanner(System.in);
-        while(!(buff == "1" || buff == "0")) {
-            System.out.println("Введите пол работника (0 - М, 1 - Ж)");
-            buff = in.nextLine();
-        }
-        gender = Integer.parseInt(buff);
-        // изменить пол
+
     }
 
     public static int get_gender()
     {
-        // добавить транзакция
-        Integer customer_id = 0;
-        System.out.println("Введите id работника");
-        customer_id = input_int("table_1", "customer_id", "int");
-        // получить пол сотрудника
+        return 0;
     }
     /////////////
     public static void get_mcc()
     {
         // добавить транзакция
-
     }
-
+    public static void add_mcc()
+    {
+        // добавить транзакция
+    }
+    public static void kill_mcc()
+    {
+        // добавить транзакция
+    }
+    public static void update_mcc()
+    {
+        // добавить транзакция
+    }
     public static int set_mcc()
     {
         // добавить транзакция
+        return 0;
     }
     public static void set_mcc_description(int code)
     {
         // добавить транзакция
     }
+    public static void load_mcc()
+    {
 
+    }
+    public static void save_mcc()
+    {
+
+    }
+
+    public static void add_people()
+    {
+
+    }
+    public static void kill_people()
+    {
+
+    }
+    public static void update_people()
+    {
+
+    }
+    public static void get_people()
+    {
+
+    }
+    public static void load_people()
+    {
+
+    }
+    public static void save_people()
+    {
+
+    }
+
+    public static Integer select_menu()
+    {
+        Scanner in = new Scanner(System.in);
+        // меню
+        System.out.println("Меню");
+        System.out.println("Выберите действие (первая цифра номер таблицы, вторая - номер действия");
+        System.out.println("Таблицы");
+        System.out.println("1 - таблица Транзакций");
+        System.out.println("2 - таблица Кодов МСС");
+        System.out.println("3 - таблица Сотрудников");
+        System.out.println("4 - таблица Сотрудников");
+        System.out.println("5 - таблица Отчет");
+        System.out.println("Действия");
+        System.out.println("1. Добавить");
+        System.out.println("2. Удалить по id");
+        System.out.println("3. Изменить по id");
+        System.out.println("4. Получить по id");
+        System.out.println("5. Загрузить из файла");
+        System.out.println("6. Сохранить в файл");
+        System.out.println("7. Запрос 1");
+        System.out.println("8. Запрос 2");
+        System.out.println("9  Запрос 3");
+        System.out.println("0. Запрос 4");
+        String menu = "";
+        Integer n = 0;
+        while(true) {
+            n = in.nextInt();
+            break;
+        }
+        return n;
+    }
+    ///////////////
     public static void main(String[] args) {
-
         try {
             Customer customer = getCustomer(1);
         } catch (NonExistentEntityException ex) {
             LOGGER.log(Level.WARNING, ex.getMessage());
         }
-
-        String table = select_table();
-
+        while(true) {
+            Integer menu = select_menu();
+            System.out.println("1. Добавить");
+            System.out.println("2. Удалить по id");
+            System.out.println("3. Изменить по id");
+            System.out.println("4. Получить по id");
+            System.out.println("5. Загрузить из файла");
+            System.out.println("6. Сохранить в файл");
+            System.out.println("7. Изменить пол");
+            System.out.println("8. Узнать пол");
+            System.out.println("9. Запрос 1");
+            System.out.println("10. Запрос 2");
+            System.out.println("11. Запрос 3");
+            System.out.println("12. Запрос 4");
+            System.out.println("0. Выход");
+            switch (menu)
+            {
+                case 11:
+                    add_trans();
+                    break;
+                case 12:
+                    kill_trans();
+                    break;
+                case 13:
+                    update_trans();
+                    break;
+                case 14:
+                    get_trans();
+                    break;
+                case 15:
+                    load_trans();
+                    break;
+                case 16:
+                    save_trans();
+                    break;
+                case 21:
+                    add_mcc();
+                    break;
+                case 22:
+                    kill_mcc();
+                    break;
+                case 23:
+                    update_mcc();;
+                    break;
+                case 24:
+                    get_mcc();
+                    break;
+                case 25:
+                    load_mcc();
+                    break;
+                case 26:
+                    save_mcc();
+                    break;
+                case 31:
+                    add_people();
+                    break;
+                case 32:
+                    kill_people();
+                    break;
+                case 33:
+                    update_people();
+                    break;
+                case 34:
+                    get_people();
+                    break;
+                case 35:
+                    load_people();
+                    break;
+                case 36:
+                    save_people();
+                    break;
+                case 47:
+                    //
+                    break;
+                case 48:
+                    //
+                    break;
+                case 49:
+                    //
+                    break;
+                case 40:
+                    //
+                    break;
+                case 0:
+                    save_people();
+                    save_trans();
+                    save_mcc();
+                    return;
+                default:
+                    System.out.println("Некорректный ввод");
+            }
+            break;
+        }
         //Test whether a customer can be added to the database
         Customer firstCustomer = new Customer("Manuel", "Kelley", "ManuelMKelley@jourrapide.com");
         Customer secondCustomer = new Customer("Joshua", "Daulton", "JoshuaMDaulton@teleworm.us");
